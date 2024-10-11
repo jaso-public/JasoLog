77c0ec3e46851380c7889a826c867df2334cf329
jaso Sorenson <jaso@cs.wisc.edu>
2024-10-10 22:12:59 -0500

    Made simple subscriber for log messages

--- src/main/java/jaso/log/SimpleLogServer.java
+++ src/main/java/jaso/log/SimpleLogServer.java
@@ -8,9 +8,9 @@ import io.grpc.ServerBuilder;
 public class SimpleLogServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(50051)
-                .addService(new LogServiceImpl(null))
-                .build();
+		LogDataStore lds = new LogDataStore();
+		
+        Server server = ServerBuilder.forPort(50051).addService(new LogServiceImpl(lds)).build();
 
         System.out.println("Starting server...");
         server.start();
---
+++ src/main/java/jaso/log/SimpleSubscriber.java
@@ -0,0 +1,62 @@
+package jaso.log;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import jaso.log.protocol.Event;
+import jaso.log.protocol.LogRequest;
+import jaso.log.protocol.LogServiceGrpc;
+import jaso.log.protocol.Request;
+import jaso.log.protocol.SubscribeRequest;
+
+public class SimpleSubscriber {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create a channel to connect to the server
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        // Create a stub to use the service
+        LogServiceGrpc.LogServiceStub asyncStub = LogServiceGrpc.newStub(channel);
+
+        // Latch to wait for the response
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // Call the Chat RPC and create a StreamObserver to handle responses
+        StreamObserver<Request> requestObserver = asyncStub.send(new StreamObserver<Event>() {
+            @Override
+            public void onNext(Event event) {
+                // Handle each response from the server
+                System.out.println("Received from server: " + event);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                // Handle errors
+                throwable.printStackTrace();
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                // Server has finished sending messages
+                System.out.println("Server has completed sending messages.");
+                latch.countDown();
+            }
+        });
+        
+        SubscribeRequest request = SubscribeRequest.newBuilder().build();
+        
+        // Respond to the client with a ChatResponse message
+        requestObserver.onNext(Request.newBuilder().setSubscribeRequest(request).build());
+
+        latch.await();
+        
+        // Shutdown the channel
+        channel.shutdown();
+    }
+}
